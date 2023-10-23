a9da39307aad664a0125672f18d0bf3b83a05ebc
Jacob Jung1~ <jacobj@live.ca>
2023-10-23 03:21:55 -0400

    Update Card

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,27 +1,21 @@
 import * as React from "react"
-import { useState } from "react";
+
 import { cn } from "@/lib/utils"
 
-const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => {
-    const [isHovered, setIsHovered] = useState(false);
-
-    return (
-      <div
-        ref={ref}
-        className={cn(
-          "rounded-xl border bg-card text-card-foreground",
-          isHovered && "shadow-2xl",
-          "h-auto",
-          className
-        )}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-        {...props}
-      />
-    );
-  }
-);
+const Card = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      className
+    )}
+    {...props}
+  />
+))
+Card.displayName = "Card"
 
 const CardHeader = React.forwardRef<
   HTMLDivElement,
@@ -41,7 +35,10 @@ const CardTitle = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <h3
     ref={ref}
-    className={cn("font-semibold leading-none tracking-tight", className)}
+    className={cn(
+      "text-2xl font-semibold leading-none tracking-tight",
+      className
+    )}
     {...props}
   />
 ))
