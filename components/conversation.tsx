8bfef42703540dc0438ec29029df5e9a6fe329b9
Jacob Jung1~ <jacobj@live.ca>
2023-10-24 13:26:02 -0400

    Moving conversations old code to new page

--- components/conversation.tsx
+++ components/conversation.tsx
@@ -6,78 +6,99 @@ import { UserButton } from "@clerk/nextjs";
 import { Comfortaa } from "next/font/google";
 import { BookOpenText   } from "lucide-react";
 
+import { zodResolver } from "@hookform/resolvers/zod";
 //Local Imports
 import { cn } from "@/lib/utils";
 import { MobileSidebar } from "@/components/mobile-sidebar";
 import { ModeToggle } from "./mode-toggle";
 import * as React from "react"
 import * as z from "zod";
-import { MessagesSquare, Plus, Calculator, CreditCard, AlarmCheck, Settings, Smile, User  } from "lucide-react";
+import { MessageSquare, Plus, Calculator, CreditCard, AlarmCheck, Settings, Smile, User  } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { Heading } from "@/components/heading";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 //Named @hookform not an address
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
-
+import { formSchema } from "@/components/coversation-auth";
 const font = Comfortaa({ weight: "600", subsets: ["latin"] });
 
 
-export const Navbar = () => {
-    return (
-        <div className="mt-24">
-        <Heading
-            title="Conversation"
-            description="Ask your friend a question"
-            icon={MessagesSquare }
-        />
-        
-              <div className="px-4 lg:px-8">
-            <div>
-                <Form {...form}>
-                    <form
-                        onSubmit={form.handleSubmit(onSubmit)}
-                        className="
-                            rounded-lg
-                            border
-                            w-full
-                            p-4
-                            px-3
-                            md:px-6
-                            focus-within:shadow-sm
-                            grid
-                            grid-cols-12
-                            gap-2
-                            "
-                            >
-                                <FormField 
-                                name="prompt"
-                                render={({ field }) => (
-                                    <FormItem className="col-span-12 lg:col-span-10">
-                                    <FormControl className="m-0 p-0">
-                                      <Input
-                                        className="
-                                        border-0 
-                                        outline-none 
-                                        focus-visible:ring-0 
-                                        focus-visible:ring-transparent
-                                        "
-                                        disabled={isLoading} 
-                                        placeholder="Whats the binary number for 13?" 
-                                        {...field}
-                                      />
-                                    </FormControl>
-                                  </FormItem>
-                                )}
-                            />
-                            <Button className="col-span-12 lg:col-span-2 w-full" type="submit" disabled={isLoading} size="icon">
-                                Generate
-                            </Button>
-                    </form>
-                </Form>
-            </div>
 
-        </div>
-        </div>
-    );
-};
+
+
+export const Conversation = () => {
+    const form = useForm<z.infer<typeof formSchema>>({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        prompt: ""
+      }
+    });
+  
+    const isLoading = form.formState.isSubmitting;
+    
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+
+    }
+  
+    return ( 
+      <div>
+        <Heading
+          title="Conversation"
+          description="Our most advanced conversation model."
+          icon={MessageSquare}
+          iconColor="text-violet-500"
+          bgColor="bg-violet-500/10"
+        />
+        <div className="px-4 lg:px-8">
+          <div>
+            <Form {...form}>
+              <form 
+                onSubmit={form.handleSubmit(onSubmit)} 
+                className="
+                  rounded-lg 
+                  border 
+                  w-full 
+                  p-4 
+                  px-3 
+                  md:px-6 
+                  focus-within:shadow-sm
+                  grid
+                  grid-cols-12
+                  gap-2
+                "
+              >
+                <FormField
+                  name="prompt"
+                  render={({ field }) => (
+                    <FormItem className="col-span-12 lg:col-span-10">
+                      <FormControl className="m-0 p-0">
+                        <Input
+                          className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                          disabled={isLoading} 
+                          placeholder="How do I calculate the radius of a circle?" 
+                          {...field}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <Button className="col-span-12 lg:col-span-2 w-full" type="submit" disabled={isLoading} size="icon">
+                  Generate
+                </Button>
+              </form>
+            </Form>
+          </div>
+          <div className="space-y-4 mt-4">
+
+           
+           
+                
+                  <p className="text-sm">
+       
+                  </p>
+                </div>
+            </div>
+          </div>
+     );
+  }
