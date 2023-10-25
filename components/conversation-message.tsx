db6c2ef5ec23ede50cde35325ef8740aa0a52b51
Jacob Jung1~ <jacobj@live.ca>
2023-10-25 02:38:01 -0400

    For current chatbot message

---
+++ components/conversation-message.tsx
@@ -0,0 +1,60 @@
+import { BeatLoader } from "react-spinners";
+import { Copy, BookOpenText } from "lucide-react"; // Import BookOpenText
+import { useTheme } from "next-themes";
+
+import { cn } from "@/lib/utils";
+import { BotAvatar } from "@/components/bot-avatar";
+import { UserAvatar } from "@/components/user-avatar";
+import { Button } from "@/components/ui/button";
+
+export interface ChatMessageProps {
+  role: "system" | "user",
+  content?: string;
+  isLoading?: boolean;
+}
+
+export const ChatMessage = ({
+  role,
+  content,
+  isLoading,
+}: ChatMessageProps) => {
+  const { theme } = useTheme();
+
+  const onCopy = () => {
+    if (!content) {
+      return;
+    }
+
+    navigator.clipboard.writeText(content);
+  }
+
+  return (
+    <div className={cn(
+      "group flex items-start gap-x-3 py-4 w-full",
+      role === "user" && "justify-end"
+    )}>
+      {role !== "user" && !isLoading && (
+        <div className="w-8 h-8"> {/* Assuming BotAvatar uses a fixed size */}
+          <BookOpenText className="w-8 h-8" /> {/* Render the BookOpenText icon */}
+        </div>
+      )}
+      <div className="rounded-md px-4 py-2 max-w-sm text-sm bg-primary/10">
+        {isLoading 
+          ? <BeatLoader color={theme === "light" ? "black" : "white"} size={5} /> 
+          : content
+        }
+      </div>
+      {role === "user" && <UserAvatar />}
+      {role !== "user" && !isLoading && (
+        <Button 
+          onClick={onCopy} 
+          className="opacity-0 group-hover:opacity-100 transition" 
+          size="icon"
+          variant="ghost"
+        >
+          <Copy className="w-4 h-4" />
+        </Button>
+      )}
+    </div>
+  )
+}
