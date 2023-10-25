1b8c5e99958762ab50b627a61e49b07e8bcab5fb
Jacob Jung1~ <jacobj@live.ca>
2023-10-25 02:38:13 -0400

    For previous chatbot messages

---
+++ components/conversation-messages.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { ElementRef, useEffect, useRef, useState } from "react";
+
+import { ChatMessage, ChatMessageProps } from "@/components/conversation-message";
+
+interface ChatMessagesProps {
+  messages: ChatMessageProps[];
+  isLoading: boolean;
+}
+
+export const ChatMessages = ({
+  messages = [],
+  isLoading,
+}: ChatMessagesProps) => {
+  const scrollRef = useRef<ElementRef<"div">>(null);
+
+  const [fakeLoading, setFakeLoading] = useState(messages.length === 0 ? true : false);
+
+  //one second of fake loading
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setFakeLoading(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, []);
+
+  useEffect(() => {
+    scrollRef?.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messages.length]);
+
+  return (
+    <div className="flex-1 overflow-y-auto pr-4">
+      <ChatMessage
+        isLoading={fakeLoading}
+        role="system"
+        content={`Hello`}
+      />
+      {messages.map((message) => (
+        <ChatMessage
+          key={message.content}
+          content={message.content}
+          role={message.role}
+        />
+      ))}
+      {isLoading && (
+        <ChatMessage
+          role="system"
+          isLoading
+        />
+      )}
+      <div ref={scrollRef} />
+    </div>
+  );
+};
