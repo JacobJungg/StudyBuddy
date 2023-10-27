4a83ed9cd6b5c0a59fad11ee7dfbc5e9f619b653
Jacob Jung1~ <jacobj@live.ca>
2023-10-27 03:19:17 -0400

    Added ChatBot page

---
+++ app/(root)/(routes)/chatbot/page.tsx
@@ -0,0 +1,15 @@
+/**
+ * StudyBuddy Jacob Jung
+ * Main page contents import
+ */
+
+import * as React from "react"
+const RootPage = () => {
+  return (
+    <div>
+      chatbot
+    </div>
+  )
+}
+
+export default RootPage;
