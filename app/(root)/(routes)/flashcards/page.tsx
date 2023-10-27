ba1b6023c198c663f9683808f6a4c832de954395
Jacob Jung1~ <jacobj@live.ca>
2023-10-27 03:19:26 -0400

    Added Flashcards page

---
+++ app/(root)/(routes)/flashcards/page.tsx
@@ -0,0 +1,15 @@
+/**
+ * StudyBuddy Jacob Jung
+ * Flashcards page
+ */
+
+import * as React from "react"
+const RootPage = () => {
+  return (
+    <div>
+      Flashcards
+    </div>
+  )
+}
+
+export default RootPage;
