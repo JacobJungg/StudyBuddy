e17983acec7f516a70bcf00ece93a39ff03c6f54
Jacob Jung1~ <jacobj@live.ca>
2023-10-27 03:18:52 -0400

    Removed chatclient from homepage (added to layout)

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,17 +1,15 @@
 /**
  * StudyBuddy Jacob Jung
- * Main page contents import
+ * Homepage
  */
 
-"use client";
+
 
 import * as React from "react"
-import { ChatClient } from "@/components/chat/main-chat";
-
 const RootPage = () => {
   return (
-    <div className="h-full p-4 space-y-2">
-  <ChatClient />
+    <div>
+      Home
     </div>
   )
 }
