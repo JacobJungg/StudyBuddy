bf9c16ad916f7c15c73885f28f9f3151d531daac
Jacob Jung1~ <jacobj@live.ca>
2023-10-27 03:19:33 -0400

    Added Quizzes page

---
+++ app/(root)/(routes)/quizzes/page.tsx
@@ -0,0 +1,15 @@
+/**
+ * StudyBuddy Jacob Jung
+ * Quizzes page
+ */
+
+import * as React from "react"
+const RootPage = () => {
+  return (
+    <div>
+      Quizzes
+    </div>
+  )
+}
+
+export default RootPage;
