28a9548fa25c5e15654dbd2c1889512ce7580e17
Jacob Jung1~ <jacobj@live.ca>
2023-10-27 04:00:24 -0400

    Added comments and organized

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,8 @@
+/**
+ * StudyBuddy Jacob Jung
+ * Creating Navbar
+ */
+
 "use client";
 
 //Global Imports
@@ -13,33 +18,27 @@ import { ModeToggle } from "./mode-toggle";
 
 
 const font = Comfortaa({ weight: "600", subsets: ["latin"] });
-interface NavbarProps {
-  isPro: boolean;
-}
-//                <<Menu className="block md:hidden" />>
 export const Navbar = () => {
     return (
         <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 h-16 border-b border-primary/10 bg-secondary">
             <div className="flex items-center">          
                 <MobileSidebar />
                 <Link href="/">
-                 <h1 className={cn("hidden md:flex text-xl md:text-3xl font-bold text-primary", font.className)}>
-                    <span className="flex items-center">
+                    <h1 className={cn("hidden md:flex text-xl md:text-3xl font-bold text-primary", font.className)}>
+                        <span className="flex items-center">
                             <BookOpenText  /> 
-                            <span className="ml-0">StudyBuddy</span>
+                            <span className="ml-0">
+                                StudyBuddy
+                            </span>
                         </span>
                     </h1>
-              </Link>
+                </Link>
             </div>
-
             <div className="flex items-center gap-x-3">
-
-
                 <ModeToggle />        
                 <UserButton />
-
-                </div>    
-                </div>       
+            </div>    
+        </div>       
 
     );
 };
