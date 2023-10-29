8d4ba8561fd1002ba97864c3bc76a15d5c2dfb0e
Jacob Jung1~ <jacobj@live.ca>
2023-10-29 03:48:07 -0400

    Added icons to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,6 +5,11 @@
 
 "use client";
 
+
+import { Home, FileText, MenuSquare, Settings,  } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+
+
 //Global Imports
 import Link from "next/link";
 import { UserButton } from "@clerk/nextjs";
@@ -19,8 +24,55 @@ import { ModeToggle } from "./mode-toggle";
 
 const font = Comfortaa({ weight: "600", subsets: ["latin"] });
 export const Navbar = () => {
+
+    const pathname = usePathname();
+    const router = useRouter();
+
+
+    const onNavigate = (url: string) => {
+          return router.push(url);
+        }
+
+    const routes = [
+        {
+          icon: Home,
+          href: '/',
+          label: "Home",
+        },
+        {
+          icon: MenuSquare  ,
+          href: '/flashcards',
+          label: "Flashcards",
+        },
+        {
+          icon: FileText ,
+          href: '/quizzes',
+          label: "Quizzes",
+        },
+        {
+          icon: BookOpenText,
+          href: '/chatbot',
+          label: "ChatBot",
+        },
+        {
+          icon: Settings,
+          href: '/settings',
+          label: "Settings",
+        },
+      ];
+
+
+
+
+
+
+
+
+
+
+
     return (
-        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 h-16 border-b border-primary/10 bg-secondary">
+        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 h-16 ">
             <div className="flex items-center">          
                 <MobileSidebar />
                 <Link href="/">
@@ -34,6 +86,42 @@ export const Navbar = () => {
                     </h1>
                 </Link>
             </div>
+
+
+
+            <div className="flex flex-col h-full text-primary bg-secondary rounded-lg"> 
+            <div className="p-3 flex-1 flex flex-wrap justify-center">
+            <div className="space-y-2">
+        <div className="flex flex-row space-x-2">
+              {routes.map((route) => (
+                <div
+                  onClick={() => onNavigate(route.href)}
+                  key={route.href}
+                  className={cn(
+                    "text-muted-foreground text-xs group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-primary hover:bg-primary/10 rounded-lg transition",
+                    pathname === route.href && "bg-primary/10 text-primary",
+                  )}
+                >
+                  <div className="flex flex-col gap-y-2 items-center flex-1">
+                    
+                    <route.icon className="h-5 w-5" />
+                    {route.label}
+                  </div>
+                </div>
+              ))}
+              </div>
+            </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
             <div className="flex items-center gap-x-3">
                 <ModeToggle />        
                 <UserButton />
