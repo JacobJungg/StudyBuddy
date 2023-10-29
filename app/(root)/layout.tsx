7285c2f41eef3fcf007dbf9383bcb3a6880f9631
Jacob Jung1~ <jacobj@live.ca>
2023-10-29 03:53:48 -0400

    Removed sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -27,12 +27,14 @@ const RootLayout = async ({
   //Navbar fill 100% vertical space of parent container
   //Sidebar being set to fill horizontal space and disapear
   //when on smaller screens eg.mobile
+
+//<div className="hidden md:flex mt-16 h-full w-20 flex-col fixed inset-y-0">
+//  <Sidebar />
+//</div>
   return ( 
     <div className="h-full">
       <Navbar />
-      <div className="hidden md:flex mt-16 h-full w-20 flex-col fixed inset-y-0">
-        <Sidebar />
-      </div>
+      
       <main className="md:pl-20 pt-16 h-full">
         {children} 
         <div className="fixed bottom-4 right-4">
