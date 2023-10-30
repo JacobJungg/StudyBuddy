6b41d77392dd61681eddb844c1b3f43607bb5fd1
Jacob Jung1~ <jacobj@live.ca>
2023-10-30 02:29:06 -0400

    New login page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,32 +1,14 @@
-/**
- * StudyBuddy Jacob Jung
- * 
- * Landing page if havnt signed in
- * Add clerk form for sign up in middle of screen
- * Will redirect to [[...sign-in]] or [[...sign-up]]
- */
 
-import { Navbar } from "@/components/navbar"
-import { Sidebar } from "@/components/sidebar";
-
-const AuthLayout = ({
-  children
-}: {
-  children: React.ReactNode;
-}) => {
-  return ( 
-    <div className="h-full">
-      <Navbar />
-      <div className="hidden md:flex mt-16 h-full w-20 flex-col fixed inset-y-0">
-      <Sidebar />
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex h-full w-full">
+      <div className="h-full w-1/2">
       </div>
-      <main className="md:pl-10 pr-10 pt-16 h-full">
-        <div className="flex justify-center items-center h-full">
-          {children}
-        </div>
-      </main>
+      <div className="h-full w-1/2 flex justify-center items-center bg-secondary">
+        {children}
+      </div>
     </div>
-   );
-}
- 
+  );
+};
+
 export default AuthLayout;
