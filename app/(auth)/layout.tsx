71b78803e311cbc1d8113b02cecb8281e3a8e5ad
Jacob Jung1~ <jacobj@live.ca>
2023-11-05 18:43:49 -0500

    Added ModeToggle to landing page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/mode-toggle";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -5,6 +6,14 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
       <div className="h-full w-1/2">
       </div>
       <div className="h-full w-1/2 flex justify-center items-center bg-secondary">
+      <div
+          className="absolute top-4 right-4 bg-secondary text-white py-2 px-4 rounded"
+         >
+
+          <ModeToggle />
+        </div>
+        
+        
         {children}
       </div>
     </div>
