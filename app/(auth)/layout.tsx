7269e2607f1e0e5eeb60e77965089c304abeea4a
Jacob Jung1~ <jacobj@live.ca>
2023-11-05 19:17:28 -0500

    Added Title and Subtitle to landing page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,16 +4,16 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex h-full w-full">
       <div className="h-full w-1/2">
-      </div>
-      <div className="h-full w-1/2 flex justify-center items-center bg-secondary">
-      <div
-          className="absolute top-4 right-4 bg-secondary text-white py-2 px-4 rounded"
-         >
-
-          <ModeToggle />
+      <div className="absolute bottom-10 left-10 text-left">
+          <h2 className="text-5xl font-bold">StudyBuddy</h2>
+          <p className="text-4xl">Dive Into Learning</p>
         </div>
-        
-        
+      </div>
+      
+      <div className="h-full w-1/2 flex justify-center items-center bg-secondary">
+        <div className="absolute top-4 right-4 bg-secondary py-2 px-4 rounded">
+          <ModeToggle />
+        </div>        
         {children}
       </div>
     </div>
