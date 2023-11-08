0f212d7e6adaa7359d1bceb8f442d88316da8567
Jacob Jung1~ <jacobj@live.ca>
2023-11-07 23:02:51 -0500

    Changed main screen text to white

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,19 +1,16 @@
-import { ModeToggle } from "@/components/mode-toggle";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex h-full w-full">
       <div className="h-full w-1/2" style={{ background: "linear-gradient(135deg, #FFA500, #FF6347)" }}>
       <div className="absolute bottom-10 left-10 text-left">
-          <h2 className="text-5xl font-bold">StudyBuddy</h2>
-          <p className="text-4xl">Dive Into Learning</p>
+          <h2 className="text-5xl font-bold text-white">StudyBuddy</h2>
+          <p className="text-4xl text-white">Dive Into Learning</p>
         </div>
       </div>
       
       <div className="h-full w-1/2 flex justify-center items-center bg-secondary">
-        <div className="absolute top-4 right-4 bg-secondary py-2 px-4 rounded">
-          <ModeToggle />
-        </div>        
+     
         {children}
       </div>
     </div>
