a8e0dcc74b7ae25d98a197e69f131d32e22c9b2e
Jacob Jung1~ <jacobj@live.ca>
2023-11-10 17:39:33 -0500

    Using Zod to validate objects from json file

---
+++ app/(root)/(routes)/quizzes/data/schema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod"
+
+// We're keeping a simple non-relational schema here.
+// IRL, you will have a schema for your data models.
+export const quizSchema = z.object({
+  id: z.string(),
+  title: z.string(),
+  status: z.string(),
+  label: z.string(),
+  author: z.string(),
+})
+
+export type Quiz = z.infer<typeof quizSchema>
