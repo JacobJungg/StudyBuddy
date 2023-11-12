6c38b1c92b975a05ad951c0dfb9ba17473711b05
Jacob Jung1~ <jacobj@live.ca>
2023-11-12 01:17:12 -0500

    Removed import

--- app/(root)/(routes)/quizzes/page.tsx
+++ app/(root)/(routes)/quizzes/page.tsx
@@ -1,7 +1,6 @@
 import { promises as fs } from "fs"
 import path from "path"
 import { Metadata } from "next"
-import Image from "next/image"
 import { z } from "zod"
 
 import { columns } from "@/components/quizzes/columns"
