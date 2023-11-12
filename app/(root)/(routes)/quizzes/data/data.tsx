4fc8425854e5cd3a11f8eeaf94c02a405cd24376
Jacob Jung1~ <jacobj@live.ca>
2023-11-12 01:22:53 -0500

    Formatting

--- app/(root)/(routes)/quizzes/data/data.tsx
+++ app/(root)/(routes)/quizzes/data/data.tsx
@@ -7,65 +7,65 @@ import {
   Signal
 } from 'lucide-react';
 
-  export const labels = [
-    {
-      value: "math",
-      label: "Math",
-    },
-    {
-      value: "english",
-      label: "English",
-    },
-    {
-      value: "history",
-      label: "History",
-    },
-    {
-      value: "science",
-      label: "Science",
-    },
-    {
-      value: "art",
-      label: "Art",
-    },
-    {
-      value: "law",
-      label: "Law",
-    },
-  ]
+export const labels = [
+{
+  value: "math",
+  label: "Math",
+  },
+  {
+    value: "english",
+    label: "English",
+  },
+  {
+    value: "history",
+    label: "History",
+  },
+  {
+    value: "science",
+    label: "Science",
+  },
+  {
+    value: "art",
+    label: "Art",
+  },
+  {
+    value: "law",
+    label: "Law",
+  },
+]
+
+export const difficulties = [
+  {
+    value: "easy",
+    label: "Easy",
+    icon: SignalLow,
+  },
+  {
+    value: "fair",
+    label: "Fair",
+    icon: SignalHigh,
+  },
+  {
+    value: "hard",
+    label: "Hard",
+    icon: Signal,
+  },
+]
   
-  export const difficulties = [
-    {
-      value: "easy",
-      label: "Easy",
-      icon: SignalLow,
-    },
-    {
-      value: "fair",
-      label: "Fair",
-      icon: SignalHigh,
-    },
-    {
-      value: "hard",
-      label: "Hard",
-      icon: Signal,
-    },
-  ]
-  
-  export const authors = [
-    {
-      label: "Student",
-      value: "student",
-      icon: GraduationCap,
-    },
-    {
-      label: "Teacher",
-      value: "teacher",
-      icon: Apple,
-    },
-    {
-      label: "StudyBuddy",
-      value: "studybuddy",
-      icon: BookOpenText,
-    },
-  ]
+export const authors = [
+  {
+    label: "Student",
+    value: "student",
+    icon: GraduationCap,
+  },
+  {
+    label: "Teacher",
+    value: "teacher",
+    icon: Apple,
+  },
+  {
+    label: "StudyBuddy",
+    value: "studybuddy",
+    icon: BookOpenText,
+  },
+]
