f8597aa31f2402825880c47c18be0f9245c9faa6
Jacob Jung1~ <jacobj@live.ca>
2023-11-14 15:45:11 -0500

    Moved to components/mainstructure folder

---
+++ components/mainstructure/sidebar.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+//Global Imports
+import { Home, FileText, MenuSquare, Settings, BookOpenText } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+export const Sidebar = () => {
+    const pathname = usePathname();
+    const router = useRouter();
+
+
+    const onNavigate = (url: string) => {
+          return router.push(url);
+        }
+
+    const routes = [
+        {
+          icon: Home,
+          href: '/',
+          label: "Home",
+        },
+        {
+          icon: MenuSquare  ,
+          href: '/flashcards',
+          label: "Flashcards",
+        },
+        {
+          icon: FileText ,
+          href: '/quizzes',
+          label: "Quizzes",
+        },
+        {
+          icon: BookOpenText,
+          href: '/chatbot',
+          label: "ChatBot",
+        },
+        {
+          icon: Settings,
+          href: '/settings',
+          label: "Settings",
+        },
+      ];
+
+      return (
+        <div className="space-y-4 flex flex-col h-full text-primary bg-secondary">
+          <div className="p-3 flex-1 flex justify-center">
+            <div className="space-y-2">
+              {routes.map((route) => (
+                <div
+                  onClick={() => onNavigate(route.href)}
+                  key={route.href}
+                  className={cn(
+                    "text-muted-foreground text-xs group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-primary hover:bg-primary/10 rounded-lg transition",
+                    pathname === route.href && "bg-primary/10 text-primary",
+                  )}
+                >
+                  <div className="flex flex-col gap-y-2 items-center flex-1">
+                    <route.icon className="h-5 w-5" />
+                    {route.label}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+    );
+};
